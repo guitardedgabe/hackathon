841326ac9b0bcf72bf101f718526990cc298c77e
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-06-23 23:21:33 -0400

    buckets submit changes

--- public/js/view/SubmitBucketView.js
+++ public/js/view/SubmitBucketView.js
@@ -3,14 +3,18 @@ $(function() {
 		
 		el: $("#createBucket"),
 
-		/*events: {
+		events: {
 			"click #submitBucket": "submitBucket"
-		},*/
+		},
 
 		initialize: function() {
+            //load user model from the given options
+            this.user = this.options.user;
+
 			this.text = this.$("#bucketText");
 			this.title = this.$("#bucketTitle");
 			this.category = this.$("#bucketCategory");
+			this.picture = this.$("#photoUpload");
 		},
 
 		submitBucket: function() {
@@ -18,12 +22,17 @@ $(function() {
 			bucketOptions.text = this.text.val();
 			bucketOptions.title = this.title.val();
 			bucketOptions.category = this.category.val();
+			bucketOptions.pictures = [this.picture.attr('src')];
+
 			//bucketOptions = selected picture
 			//bucketOptions.author = user.get("name");
-			bucketOptions.author = "Gabriel Busto";
-			var bucket = new Bucket(bucketOptions);
-			buckets.addOne(bucket);
+            
+			bucketOptions.author = this.user.id;
+
+			this.collection.create(bucketOptions);
+            
 		}
 
 	});
+
 });
